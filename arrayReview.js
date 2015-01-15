35ae7e68d46a7c547abd70687a2bd31c2b00a543
Ty Frith <tyfrith@gmail.com>
2015-01-15 09:06:07 -0700

    updated

--- arrayReview.js
+++ arrayReview.js
@@ -2,7 +2,10 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+var last = function() {
+	return threeItems[2];
+}
+
 
 
 
@@ -13,8 +16,15 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+var oddRemover = function() {
+	for (var i = evenArray.length -1; i >= 0; i--) {
+		if (evenArray[i] % 2 !== 0) {
+			evenArray.splice(i,1);
+		};
+	};
+};
 
+oddRemover(evenArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -26,7 +36,17 @@ var getRandomArbitrary = function() {
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var finder = function(rNum, arr) {
+	var flag = false;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === rNum) {	
+			flag = true;
+		}
+  	}
+  	alert(flag);
+};
+
+finder(getRandomArbitrary(), randomArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -35,9 +55,11 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
-var second;
+var second = first.slice();
 
-  //code here
+second.push(6, 7);
+
+
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -51,7 +73,19 @@ alert(second) //[1,2,3,4,5,6,7];
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
 
-  //code here
+var longest = function() {
+	var newArray = [];
+	newArray = sentence.split(' ');
+	var word = "";
+	for (var i = 0; i < newArray.length; i++) {
+		if (newArray[i].length > word.length) {
+			word = newArry[i];
+		}
+	}
+	return word;
+}
+
+longest(sentence);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -62,7 +96,16 @@ var sentence = "Dev Mountain is the best"
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+var cap = function() {
+		myPoem = myPoem.split(' ');
+
+		for (var i = 0; i < myPoem.length; i++) {
+			myPoem
+		}
+
+	};
+
+
 
 
 
@@ -72,3 +115,33 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
